52c4e58e4ea6c3f2014a385ae1b82bb2f919ac64
mlim20 <mlim20@student.ubc.ca>
2025-01-22 21:57:00 -0800

    added mood file and mood list in tracker

--- src/model/Tracker.java
+++ src/model/Tracker.java
@@ -1,4 +1,12 @@
 package src.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tracker {
+    private List<Mood> moodList;
+
+    public Tracker() {
+        moodList = new ArrayList<>();
+    }
 }
