fc24bf4ba5f329539888da03e0b5adee124c05c1
mlim20 <mlim20@student.ubc.ca>
2025-02-18 15:46:24 -0800

    updated mood to include rating and date

--- src/model/Mood.java
+++ src/model/Mood.java
@@ -1,4 +1,22 @@
 package src.model;
 
+import java.text.SimpleDateFormat;
+
 public class Mood {
+    private int rating;
+
+    private SimpleDateFormat date;
+
+    public Mood(int rating, SimpleDateFormat date) {
+        this.rating = rating;
+        this.date = date;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public SimpleDateFormat date() {
+        return date;
+    }
 }
